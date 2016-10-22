8bc0dc7b8944e8f04585ca00510acf31b0433d14
badr bellaj <bellaj.badr@gmail.com>
2016-10-22 18:32:53 +0100

    working with minor error

--- Endians/Endians.cpp
+++ Endians/Endians.cpp
@@ -6,14 +6,32 @@
 #include <iostream> 
 #include <string> 
  #include <sstream> 
+#include <iomanip>  
+
 using namespace std;
- 
+
+#include <openssl/sha.h>
+string sha256(const string str)
+{
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_CTX sha256;
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, str.c_str(), str.size());
+	SHA256_Final(hash, &sha256);
+	stringstream ss;
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+	{
+		ss << hex << setw(2) << setfill('0') << (int)hash[i];
+	}
+	return ss.str();
+}
 
 int main()
 {
- 
+
+
 	string LittleEnd;// e.g "4f8505cdaee726fa7bfd19e18da8 1f 47 4f 6c 45 3a ea 6f 7f be 09 43 d8 08 7a b9 9d 1e f7 a7 9b db d3 9e 45 83";
-	std::cout << "give your Little Endian String (hex): ";
+	std::cout << "give your Little Endian String (hex)[spaces will be removed]: ";
 	std::getline(std::cin, LittleEnd);
  
 	string::iterator it;		// The string iterator.
@@ -34,8 +52,7 @@ int main()
 			it = LittleEnd.erase(it);
 	}
 
- 
-	 
+  
  	 
  
 	 string bigEnd = LittleEnd;
@@ -49,14 +66,21 @@ int main()
 	 
 		
 	}
-	 
+	 string sha256_1 = sha256(bigEnd);
+	 string sha256_2 = sha256(sha256(bigEnd));
  
+	 std::cout << "little endian form: " << LittleEnd << endl;
+	 std::cout << "Big Endians form : " << bigEnd << endl;
+ 	 std::cout <<"sha256 (of BE): " << sha256_1 << endl;
+	 std::cout << "Double sha256 (of BE): " << sha256_2 << endl;
+	 std::cout << "The results are stored into endian.txt: " << endl;
+
   	ofstream myfile;
 	myfile.open("endian.txt");
-	myfile << bigEnd;
+	 myfile <<"little endian form"<< LittleEnd <<"\nBig Endian Form :"<< bigEnd<<"\nsha256 :"<< sha256_1<<"\ndouble sha256 :"<< sha256_2<<endl;
 	myfile.close();
 	std::getline(std::cin, LittleEnd);
-
+	 
     return 0;
 
 }
